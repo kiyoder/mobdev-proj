46d53b6c4c627304752539e4b7b1e2740fee3034
Kiyokun <yoshinori.kyonojr@cit.edu>
2024-04-22 07:33:17 +0800

    submit button onclick

--- app/src/main/java/com/example/androidprojectcollection/PassingIntentsExercise.java
+++ app/src/main/java/com/example/androidprojectcollection/PassingIntentsExercise.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+
+import android.widget.Button;
+
+import android.widget.Toast;
+
 public class PassingIntentsExercise extends AppCompatActivity {
 
+    private Button submitButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passing_intents_exercise);
+
+        submitButton = findViewById(R.id.btnSubmit);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Toast.makeText(PassingIntentsExercise.this, "Submit button clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
