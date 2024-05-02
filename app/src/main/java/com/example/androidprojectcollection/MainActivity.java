7292bc48a40b082a4adbe4f2a74f1bcdb559d2ed
Kiyokun <yoshinori.kyonojr@cit.edu>
2024-05-02 17:54:46 +0800

    added maps  button and functionalities

--- app/src/main/java/com/example/androidprojectcollection/MainActivity.java
+++ app/src/main/java/com/example/androidprojectcollection/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.androidprojectcollection;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     Button btn3;
     Button btn4;
 
-    Button btnIntents1, btnMenu;
+    Button btnIntents1, btnMenu, btnMaps;
 
 
     @Override
@@ -85,6 +86,17 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+        btnMaps = (Button) findViewById(R.id.btnMaps);
+        btnMaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intMaps = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:11.96362199175654, 121.92349818190378"));
+                startActivity(intMaps);
+            }
+
+        });
+
 
 
 
